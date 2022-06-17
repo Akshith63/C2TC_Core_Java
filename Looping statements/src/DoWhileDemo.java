5db893ff7bc788f371385efa1cadcaad2f5ba1cf
HP <HP@Akshith>
2022-06-17 20:18:47 +0530

    first commit

---
+++ Looping statements/src/DoWhileDemo.java
@@ -0,0 +1,16 @@
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=20;
+		do
+		{
+			i=i+2;
+			System.out.println(i);
+		}
+	while(i<=20);
+		System.out.println(i);
+	}
+
+}
---
+++ Looping statements/src/WhileLoopDEmo.java
@@ -0,0 +1,14 @@
+
+public class WhileLoopDEmo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=5;
+while(i<=15)
+{
+	i=i+1;
+	System.out.println(i);
+}
+	}
+
+}
